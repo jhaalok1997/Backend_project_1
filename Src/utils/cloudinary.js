d7725dbe2de8ae8e6b34c71e352b364ed965f369
Alok Kumar Jha <jhaalok1997@gmail.com>
2024-08-30 17:42:28 +0530

    configure the multer

---
+++ Src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 } from "cloudinary";
+import fs from "fs"
+
+
+cloudinary.config({ 
+    cloud_name: process.env.cloudinary_cloud_name, 
+    api_key: process.env.cloudinary_api_key, 
+    api_secret: process.env.cloudinary_api_secret // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // uploading the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has uploaded successfully
+        console.log("file  uploaded cloudinary", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+        console.error("failed to upload the file")
+    }
+}
