f9e8357cded51ce4cc189d3b035badfbb8746801
Alok Kumar Jha <jhaalok1997@gmail.com>
2024-09-04 12:54:52 +0530

    spelling error

--- Src/controllers/user.controller.js
+++ Src/controllers/user.controller.js
@@ -59,7 +59,7 @@ const registerUser = asyncHandler (async (req, res) => {
             throw new ApiError(500,"Something went wrong while registering the user .....")
         }
 
-        return res.staus(201).json(
+        return res.status(201).json(
             new ApiResponse(200,createdUser,"user registerd Successfully !!!")
         )
 
